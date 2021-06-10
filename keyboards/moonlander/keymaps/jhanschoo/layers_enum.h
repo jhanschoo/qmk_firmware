d5e4236c88830c6d164cce211ce809b0a4cf8c63
Johannes Choo <jhanschoo@gmail.com>
2021-06-10 23:31:56 +0800

    add normal layer
    Signed-off-by: Johannes Choo <jhanschoo@gmail.com>

--- keyboards/moonlander/keymaps/jhanschoo/layers_enum.h
+++ keyboards/moonlander/keymaps/jhanschoo/layers_enum.h
@@ -1,6 +1,7 @@
 enum layers {
     BASE,  // default layer
+    NORM,  // normal-like for gaming
+    GW2,  // guild wars 2
     SYMB,  // symbols
     MDIA,  // media keys
-    GAME,  // gaming layout
 };
